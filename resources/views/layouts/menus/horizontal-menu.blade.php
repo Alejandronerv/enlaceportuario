db9f9827762419e1ec13dadf9790c3cc11e4dd2b
Emmanuel Paternina <e.paternina@softd3v.com>
2024-11-30 21:44:51 -0500

    Control Menu

--- resources/views/layouts/menus/horizontal-menu.blade.php
+++ resources/views/layouts/menus/horizontal-menu.blade.php
@@ -1,16 +1,32 @@
+<?php
+$type = Session::get('type');
+?>
+
+
 <div class="horizontal-main hor-menu clearfix">
     <div class="horizontal-mainwrapper container clearfix">
         <nav class="horizontalMenu clearfix">
             <ul class="horizontalMenu-list">
                 <li aria-haspopup="true">
                     <a href="{{ route('dashboard') }}" class="sub-icon">
-                        <svg class="hor-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="26" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
+                        <svg class="hor-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="26"
+                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                            stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                            <polyline points="9 22 9 12 15 12 15 22"></polyline>
+                        </svg>
                         Dashboard
-                     </a>
+                    </a>
                 </li>
                 <li aria-haspopup="true">
                     <a href="#" class="sub-icon">
-                        <svg class="hor-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="12 2 2 7 12 12 22 7 12 2"></polygon><polyline points="2 17 12 22 22 17"></polyline><polyline points="2 12 12 17 22 12"></polyline></svg>
+                        <svg class="hor-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                            stroke-linecap="round" stroke-linejoin="round">
+                            <polygon points="12 2 2 7 12 12 22 7 12 2"></polygon>
+                            <polyline points="2 17 12 22 22 17"></polyline>
+                            <polyline points="2 12 12 17 22 12"></polyline>
+                        </svg>
                         Inquiries <i class="fa fa-angle-down horizontal-icon"></i>
                     </a>
                     <ul class="sub-menu">
@@ -29,18 +45,24 @@
                         <li><a href="widgets-2.html">Chart Widgets</a></li>
                     </ul>
                 </li> --}}
-                <li aria-haspopup="true">
-                    <a href="#" class="sub-icon">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="hor-icon"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
-                        Forms <i class="fa fa-angle-down horizontal-icon"></i>
-                    </a>
-                    <ul class="sub-menu">
-                        <li><a href="{{ route('users.table') }}"> Users</a></li>
-                        <li><a href="{{ route('announcements.table') }}"> Announcements</a></li>
-                        <li><a href="{{ route('yardinventory.table') }}"> Inventory Yard File Upload</a></li>
-                    </ul>
-                </li>
-
+                @if ($type === 1)
+                    <li aria-haspopup="true">
+                        <a href="#" class="sub-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round" class="hor-icon">
+                                <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                                <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+                            </svg>
+                            Forms <i class="fa fa-angle-down horizontal-icon"></i>
+                        </a>
+                        <ul class="sub-menu">
+                            <li><a href="{{ route('users.table') }}"> Users</a></li>
+                            <li><a href="{{ route('announcements.table') }}"> Announcements</a></li>
+                            <li><a href="{{ route('yardinventory.table') }}"> Inventory Yard File Upload</a></li>
+                        </ul>
+                    </li>
+                @endif
             </ul>
         </nav>
         <!--Nav end -->
