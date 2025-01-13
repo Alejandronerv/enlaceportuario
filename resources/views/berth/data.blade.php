e4b9930082ea57aff3b7ffcd2c82082f610840d5
Emmanuel Paternina <e.paternina@softd3v.com>
2025-01-12 20:20:11 -0500

    Data Berth Fix #8

--- resources/views/berth/data.blade.php
+++ resources/views/berth/data.blade.php
@@ -1,13 +1,23 @@
+<!DOCTYPE html>
 <html>
+<head>
+    <title>Operation Berth Graphic</title>
+    <style>
+        pre {
+            background-color: #f8f9fa;
+            padding: 10px;
+            border-radius: 5px;
+        }
+    </style>
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+</head>
 <body>
+    <h1>Operation Berth Graphic</h1>
+    <div id="timeline-advanced"></div>
+    <div id="timeline-grouped"></div>
     <script type="application/json" id="json-data">
         {!! json_encode($jsonData ?? [], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT) !!}
     </script>
-
-    <script>
-        // Example: How to parse the JSON in JavaScript
-        const jsonData = JSON.parse(document.getElementById('json-data').textContent);
-        console.log(jsonData);
-    </script>
+    <script src="{{ asset('js/berth-data.js') }}"></script>
 </body>
 </html>
