c12e26835f896c6e1d24d2dcb1df286df74da863
Emmanuel Paternina <e.paternina@softd3v.com>
2025-01-12 20:09:29 -0500

    Data Berth Fix #6

--- resources/views/berth/data.blade.php
+++ resources/views/berth/data.blade.php
@@ -1,16 +1,13 @@
-<!DOCTYPE html>
 <html>
-<head>
-    <title>Operation Berth Graphic</title>
-    <style>
-        pre {
-            background-color: #f8f9fa;
-            padding: 10px;
-            border-radius: 5px;
-        }
-    </style>
-</head>
 <body>
-    <pre>{{ $jsonData }}</pre>
+    <script type="application/json" id="json-data">
+        {!! json_encode($jsonData, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT) !!}
+    </script>
+
+    <script>
+        // Example: How to parse the JSON in JavaScript
+        const jsonData = JSON.parse(document.getElementById('json-data').textContent);
+        console.log(jsonData);
+    </script>
 </body>
 </html>
