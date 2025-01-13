e31e8f4d6d0de90144ca2e0ed506f60d4e2c0894
Emmanuel Paternina <e.paternina@softd3v.com>
2025-01-12 20:22:07 -0500

    Data Berth Fix #9

--- resources/views/berth/data.blade.php
+++ resources/views/berth/data.blade.php
@@ -1,23 +1,8 @@
-<!DOCTYPE html>
 <html>
 <head>
-    <title>Operation Berth Graphic</title>
-    <style>
-        pre {
-            background-color: #f8f9fa;
-            padding: 10px;
-            border-radius: 5px;
-        }
-    </style>
-    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+
 </head>
 <body>
-    <h1>Operation Berth Graphic</h1>
-    <div id="timeline-advanced"></div>
-    <div id="timeline-grouped"></div>
-    <script type="application/json" id="json-data">
-        {!! json_encode($jsonData ?? [], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT) !!}
-    </script>
-    <script src="{{ asset('js/berth-data.js') }}"></script>
+    <pre>{{ $jsonData }}</pre>
 </body>
 </html>
