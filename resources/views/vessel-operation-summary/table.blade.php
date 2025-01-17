f13e6472d54a5576456f4b40325a474f3bd7539f
Emmanuel Paternina <e.paternina@softd3v.com>
2025-01-16 21:55:06 -0500

    Fix Table Data

--- resources/views/vessel-operation-summary/table.blade.php
+++ resources/views/vessel-operation-summary/table.blade.php
@@ -59,9 +59,12 @@
 
                                         @foreach ($veselFields as $veselField)
                                             <tr>
-                                                <a href="{{ route('vessel-info.table', ['vesselCode' => $veselField->PO_VESSEL_CODE]) }}">
-                                                    {{ $veselField->PO_VESSEL_NAME }}
-                                                </a>
+                                                <td>
+                                                    <a
+                                                        href="{{ route('vessel-info.table', ['vesselCode' => $veselField->PO_VESSEL_CODE]) }}">
+                                                        {{ $veselField->PO_VESSEL_NAME }}
+                                                    </a>
+                                                </td>
                                                 <td>{{ $veselField->PO_COMM_DATE_TIME }}</td>
                                                 <td>{{ $veselField->PO_TTL_VALUE }}</td>
                                                 <td>{{ $veselField->PO_DISG_VALUE }}</td>
